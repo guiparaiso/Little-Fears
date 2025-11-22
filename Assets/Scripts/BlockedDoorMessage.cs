d6697f50f5320b27ace02d10c406e13cca0f7201
Vinicius Rodrigues <viniciusrf2@al.insper.edu.br>
2025-11-21 21:19:54 -0300

    Add UI message to blocked door

---
+++ Assets/Scripts/BlockedDoorMessage.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class MensagemProximidade : MonoBehaviour
+{
+    [Header("Configuração")]
+    // Arraste o objeto de Texto da sua UI para este campo no Inspector
+    public GameObject messageUI;
+
+    void Start()
+    {
+        // Garante que a mensagem comece desligada
+        messageUI.SetActive(false);
+    }
+
+    // Detecta quando o Player ENTRA na área
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            messageUI.SetActive(true);
+        }
+    }
+
+    // Detecta quando o Player SAI da área
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            messageUI.SetActive(false);
+        }
+    }
+}
